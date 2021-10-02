957d4fb81322b498d9b7ca3ef52cc57f710bcac1
Alankrit19 <82066166+Alankrit19@users.noreply.github.com>
2021-10-03 00:37:06 +0530

    Update binSearch.cpp

--- binSearch.cpp
+++ binSearch.cpp
@@ -1,3 +1,5 @@
+
+   
 #include <bits/stdc++.h>
 using namespace std;
 int binSearch(int arr[], int target, int n)
@@ -23,11 +25,55 @@ int binSearch(int arr[], int target, int n)
     }
     return -1;
 }
+//Added order agnostic binary search
+
+int agnosticBinSearch(int arr[], int target, int n)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+
+        bool isAsc = arr[0] < arr[n - 1] ? true : false;
+        if (isAsc)
+        {
+            if (arr[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] < target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
 
 
 int main()
 {
     int arr[] = {100,78,53,9,1};
     cout << binSearch(arr, 1, 5);
+    cout << agnosticBinSearch(arr, 1, 5);
+        
     return 0;
 }
